087fd63ae241d214cb80e5246570da8082c13318
Anush <afr741@mun.ca>
2019-11-15 21:03:26 +0600

    connected to mongoDB with mongoose

--- config/db.js
+++ config/db.js
@@ -4,7 +4,9 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db);
+    await mongoose.connect(db, {
+      useNewUrlParser: true
+    });
     console.log('mongoDB connected');
   } catch (err) {
     console.error(err.message);
