eca3973867d97888a61cdbb012f1674d463d1252
Anush <afr741@mun.ca>
2019-11-16 16:17:20 +0900

    Route FIles with Express Router

--- config/db.js
+++ config/db.js
@@ -5,6 +5,7 @@ const db = config.get('mongoURI');
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
+      useNewUrlParser: true,
       useUnifiedTopology: true
     });
     console.log('mongoDB connected');
